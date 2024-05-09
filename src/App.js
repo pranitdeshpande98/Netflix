33cf6309516cf060ae9bf28dc56afb911343ef4c
Pranit Deshpande <71553492+pranitdeshpande98@users.noreply.github.com>
2024-05-09 11:26:51 -0400

    Login Image and Logo added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-
-import './App.css';
-
+import Body from "./components/Body";
 function App() {
-  return (
-    <div className="App">
-      Namaste everyone
-    </div>
-  );
+  return <Body />;
+  
 }
 
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './Header'
+
+const Login = () => {
+  return (
+    <div>
+        < Header />
+        <div>
+        <img src= "https://assets.nflxext.com/ffe/siteui/vlv3/31ef2c5c-3d08-47d5-b7a9-f29e4f4f893d/3152e5c9-a0d5-495b-ab03-073a70c5d268/US-en-20240506-POP_SIGNUP_TWO_WEEKS-perspective_WEB_34f5ff74-a994-4852-b27c-f4196ec21c67_small.jpg"
+        alt = "logo" />
+        </div>
+
+    </div>
+  )
+}
+
+export default Login

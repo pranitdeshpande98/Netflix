ae5c4d9f06e3d32abbdc1b16ff87ebca99677a5d
Pranit Deshpande <71553492+pranitdeshpande98@users.noreply.github.com>
2024-05-15 09:46:30 -0400

    initial changes to browse page i.e., adding video title and video embed changes.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,3 +4,13 @@ export const SIGNOUT =
 "https://icon-library.com/images/netflix-icon-transparent/netflix-icon-transparent-29.jpg";
 export const BG_IMAGE = 
 "https://assets.nflxext.com/ffe/siteui/vlv3/31ef2c5c-3d08-47d5-b7a9-f29e4f4f893d/3152e5c9-a0d5-495b-ab03-073a70c5d268/US-en-20240506-POP_SIGNUP_TWO_WEEKS-perspective_WEB_34f5ff74-a994-4852-b27c-f4196ec21c67_small.jpg";
+
+
+  export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxY2IzNDY0YWYyNjg3OGYzOTM5OWU4YTEyMGMyYTMwYiIsInN1YiI6IjY2NDM3MmI3NTNjMmRjMTZiNjU5ZmVlMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ak4FrvC9rXfUJJl-e2AYh2IMkPGyMK4mhDoxOxQkcfI'
+    }
+  };
+  
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,15 +1,21 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"; 
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
+  apiKey: "AIzaSyD21KHNPHJfAw8ESx4lEDHLrrXAUqU3NBo",
+  authDomain: "netflixgpt-86887.firebaseapp.com",
+  projectId: "netflixgpt-86887",
+  storageBucket: "netflixgpt-86887.appspot.com",
+  messagingSenderId: "166790156879",
+  appId: "1:166790156879:web:3e25fd19afdcb902dc103a",
+  measurementId: "G-EVCHSFCY6B"
 };
 
 // Initialize Firebase
